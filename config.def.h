39ee2d1ae690de31a681ce4ec6daeefa293b5053
ErreZac <raphael.zaccone@unige.it>
2022-03-07 09:20:42 +0100

    minor things

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "JetBrainsMono Nerd Font:size=22:style:Regular:antialias=true:autohint=true";
-static char *font = "Victor Mono Nerd Font:size=24:style:Medium:antialias=true:autohint=true";
+static char *font = "Victor Mono Nerd Font:size=22:style:Regular:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
