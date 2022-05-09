2825b90b80a3ea908fa3731debc5f6085b28fe76
ErreZac <raphael.zaccone@unige.it>
2022-05-09 08:35:47 +0200

    catppuccin minor fixes

--- config.h
+++ config.h
@@ -134,38 +134,6 @@ unsigned int defaultbg = 0;
 unsigned int defaultcs = 7;
 static unsigned int defaultrcs = 0;
 
-/* gruvbox Terminal colors (16 first used in escape sequence) */
-//static const char *colorname[] = {
-//	/* 8 normal colors */
-//	"#1d2021", //#282828
-//	"#cc241d",
-//	"#98971a",
-//	"#d79921",
-//	"#458588",
-//	"#b16286", //
-//	"#689d6a", //
-//	"#a89984",
-//	/* 8 bright colors */
-//	"#928374",
-//	"#fb4934",
-//	"#b8bb26",
-//	"#fabd2f",
-//	"#83a598",
-//	"#d3869b", // F5C2E7
-//	"#8ec07c", //
-//	"#fbf1c7", // #ebdbb2
-//
-//[256] = "#fbf1c7", /* default foreground colour */
-//[257] = "#1d2021", /* default background colour */
-//[258] = "#928374", /*575268*/
-//};
-///*
-// * foreground, background, cursor, reverse cursor
-// */
-//unsigned int defaultfg = 256;
-//unsigned int defaultbg = 257;
-//unsigned int defaultcs = 258;
-//static unsigned int defaultrcs = 258;
 
 /*
  * https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h4-Functions-using-CSI-_-ordered-by-the-final-character-lparen-s-rparen:CSI-Ps-SP-q.1D81
@@ -180,7 +148,7 @@ static unsigned int defaultrcs = 0;
  * 7: blinking st cursor
  * 8: steady st cursor
  */
-static unsigned int cursorstyle = 3;
+static unsigned int cursorstyle = 1;
 static Rune stcursor = 0x2603; /* snowman ("☃") */
 
 /*
