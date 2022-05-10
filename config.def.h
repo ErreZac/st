e626881e8bdea4244d2787f5636178bdb9ccdf47
ErreZac <raphael.zaccone@unige.it>
2022-05-10 08:48:58 +0200

    onedark

--- config.def.h
+++ config.def.h
@@ -110,31 +110,30 @@ unsigned int tabspaces = 4;
 float alpha = 0.9;
 
 static const char *colorname[] = {
-  "#2E3440", /* base00 */
-  "#BF616A", /* base08 */
-  "#A3BE8C", /* base0B */
-  "#EBCB8B", /* base0A */
-  "#88C0D0", /*  base0D */
-  "#B48EAD", /* base0E */
-  "#8FBCBB", /* base0C */
-  "#E5E9F0", /* d5c4a1 base05 */
-
-  "#3B4252", /*665c54 base03 */
-  "#D08770", /* base09 */
-  "#434C5E", /* base01 */
-  "#4C566A", /* base02 */
-  "#5E81AC", /* base04 */
-  "#D8DEE9", /* base06 */
-  "#81A1C1", /* base0F */
-  "#ECEFF4", /* base07 */
-  [256] = "#0B0D10",
-  [257] = "#ECEFF4",
-  [258] = "#171A20",
-  [259] = "#0F1115",
+  "#282c34", /* base00 */
+  "#e06c75", /* base08 */
+  "#98c379", /* base0B */
+  "#e5c07b", /* base0A */
+  "#61afef", /* base0D */
+  "#c678dd", /* base0E */
+  "#56b6c2", /* base0C */
+  "#abb2bf", /* base05 */
+  "#545862", /* base03 */
+  "#d19a66", /* base09 */
+  "#353b45", /* base01 */
+  "#3e4451", /* base02 */
+  "#565c64", /* base04 */
+  "#b6bdca", /* base06 */
+  "#be5046", /* base0F */
+  "#c8ccd4", /* base07 */
+  
+  [256] = "#14161a",
+  [257] = "#c8ccd4",
+  [258] = "#0a0b0d"
 };
 
 unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultbg = 258;
 unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
 
