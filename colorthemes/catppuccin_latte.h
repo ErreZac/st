0f1cfad3acdb607649834f8457e95c80c9efb732
ErreZac <raphael.zaccone@unige.it>
2022-06-20 16:31:15 +0200

    added latte

---
+++ colorthemes/catppuccin_latte.h
@@ -0,0 +1,24 @@
+static const char *colorname[] = {
+  "#4c4f69", 
+  "#d20f39", 
+  "#40a02b", 
+  "#df8e1d", 
+  "#1e66f5", 
+  "#8839ef", 
+  "#179299", 
+  "#ccd0da", 
+
+  "#7c7f93",
+  "#fe640b",
+  "#eff1f5",
+  "#bcc0cc",
+  "#9ca0b0",
+  "#5c5f77",
+  "#e64553",
+  "#eff1f5",
+};
+
+unsigned int defaultfg = 0;
+unsigned int defaultbg = 15;
+unsigned int defaultcs = 0;
+static unsigned int defaultrcs = 15;
