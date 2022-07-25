8f6ac913325c6c02c4b44dc4e02b1bd4775cb6dc
ErreZac <raphael.zaccone@unige.it>
2022-07-25 09:20:43 +0200

    everblush, give it a try

---
+++ colorthemes/everblush.h
@@ -0,0 +1,24 @@
+static const char *colorname[] = {
+  "#232a2d", 
+  "#e57474", 
+  "#8ccf7e", 
+  "#e5c76b", 
+  "#67b0e8", 
+  "#c47fd5", 
+  "#6cbfbf", 
+  "#b3b9b8", 
+
+  "#404749",
+  "#e5c76b",
+  "#141b1e",
+  "#232a2d",
+  "#3b4244",
+  "#b3b9b8",
+  "#e57474",
+  "#dadada",
+};
+
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 10;
+unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 10;
