956f5ef48a1e527825aabb7ef5a9c723dde8f6d3
ErreZac <raphael.zaccone@unige.it>
2022-09-05 09:53:00 +0200

    gruvbox again..

--- colorthemes/gruvbox16.h
+++ colorthemes/gruvbox16.h
@@ -1,28 +1,25 @@
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#1d2021", //#282828
+	"#1d2021", 
 	"#fb4934",
 	"#b8bb26",
 	"#fabd2f",
 	"#83a598",
-	"#d3869b", // F5C2E7
+	"#d3869b", 
 	"#8ec07c", //
-	"#fbf1c7",
-	/* 8 bright colors */
+	"#ebdbb2",
+
 	"#665c54",
 	"#d79921",
+    "#0e1011",
     "#282828",
     "#3c3836",
     "#a89984",
-    "#ebdbb2",
 	"#cc241d",
-	"#fbf1c7", // #ebdbb2
-               //
-    [256] = "#0e1011", /* default background colour */
-    [257] = "#f9f5d7", /* default foreground colour */
+	"#fbf1c7", 
 };
 
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 10;
+unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 10;
