7e2ea2322aad7c71db42810028fce19bda30689d
ErreZac <raphael.zaccone@unige.it>
2022-10-27 09:36:14 +0200

    added google colorscheme

---
+++ colorthemes/google.h
@@ -0,0 +1,25 @@
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#202124", 
+	"#ea4335",
+	"#34a853",
+	"#fbbc05",
+	"#4285f4",
+	"#a142f4", 
+	"#24c1e0", //
+	"#e8eaed",
+
+	"#5f6368",
+	"#fbbc05",
+    "#202124",
+    "#5f6368",
+    "#5f6368",
+    "#e8eaed",
+	"#ea4335",
+	"#ffffff", 
+};
+
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 10;
+unsigned int defaultcs = 15;
+static unsigned int defaultrcs = 10;
