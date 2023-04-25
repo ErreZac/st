547510a6ea44a3682775a909358c71a6b563b8c3
ErreZac <raphael.zaccone@unige.it>
2023-04-25 17:35:28 +0200

    added yet another gruvbox

---
+++ colorthemes/gruvbox_bright.h
@@ -0,0 +1,28 @@
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#282828", 
+	"#fb4934",
+	"#b8bb26",
+	"#fabd2f",
+	"#83a598",
+	"#d3869b", //
+	"#8ec07c", //
+	"#a89984",
+	/* 8 bright colors */
+	"#3c3836",
+	"#fb4934",
+	"#b8bb26",
+	"#fabd2f",
+	"#83a598",
+	"#d3869b", // F5C2E7
+	"#8ec07c", //
+	"#fbf1c7", // #ebdbb2
+
+[256] = "#1d2021", /* default background colour */
+[257] = "#fbf1c7", /* default foreground colour */
+};
+
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
